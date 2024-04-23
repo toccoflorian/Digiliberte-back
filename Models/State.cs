1c6c923e98aab079f25693a8f621bd85afea24e0
Florian <t.florian181181@gmail.com>
2024-04-23 12:45:25 +0200

    [ADD] Creation models

---
+++ Models/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+
+        // Navigation For EF
+
+        public List<Vehicle>? Vehicles { get; set; }
+    }
+}
