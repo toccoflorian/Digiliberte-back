fac9bf9922bb7892b98b6e71b5a839f2b5493d4c
CedricDLMS <delmascede@gmail.com>
2024-04-23 12:35:38 +0200

    SecondCommit

---
+++ Models/Vehicle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Vehicle
+    {
+        // Variables propres à vehicule
+        public int Id { get; set; }
+        public string Immatriculation { get; set; }
+        public int ColorId { get; set; }
+
+        // Localization 
+
+        public int LocalizationID { get; set; }
+        public Localization Localization { get; set; }
+
+        // Motorization
+
+        public int MotorizationID { get; set; }
+        public Motorization Motorization { get; set; }
+
+        // Model
+
+        public int ModelID { get; set; }
+        public Model Model { get; set; }
+
+        // State
+
+        public int StateID { get; set; }
+        public State State { get; set; }
+
+        // Category 
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+        // Brand 
+
+        public int BrandID { get; set; }
+        public Brand Brand { get; set; }
+
+        // Rents
+
+        public List<Rent>? Rents { get; set; }
+
+        // Picture
+
+        public string PictureURL { get; set; }
+
+
+    }
+}
--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b949c94da92863efa6dc7e2905ccfe87949c729b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c72e674774437aa896acd414e265e8f7b35fce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
