0f682a86f7ca99e9a2710d86a084b35c91ef4deb
Florian <t.florian181181@gmail.com>
2024-04-23 12:43:40 +0200

    [ADD] Creation models

---
+++ Models/Date.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Date
+    {
+        public int Id;
+
+        public DateTime Date {  get; set; }
+        public List<Rent>? StartDateRents {  get; set; }
+        public List<Rent>? EndDateRents { get; set; }
+        public List<CarPool>? StartDateCarPools {  get; set; }
+        public List<CarPool>? EndDateCarPools {  get; set; }
+    }
+}
