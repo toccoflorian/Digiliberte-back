fdcd45839afb3e498609c9967444a676d4d76249
Florian <t.florian181181@gmail.com>
2024-04-23 14:04:26 +0200

    [ADD] re-init

--- Models/Rent.cs
+++ Models/Rent.cs
@@ -12,9 +12,9 @@ namespace Models
         [Key]
         public int Id { get; set; }
         public int StartDateID { get; set; }
-        public DateTime StartDate { get; set; }
+        public Date StartDate { get; set; }
         public int ReturnDateID { get; set; }
-        public DateTime ReturnDate { get; set; }
+        public Date ReturnDate { get; set; }
         public string UserID { get; set; }
         public User User { get; set; }
         public Vehicle Vehicle {  get; set; }
