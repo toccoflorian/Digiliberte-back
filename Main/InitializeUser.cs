6747657175a3af9202a8228bb05fb60467061615
CedricDLMS <delmascede@gmail.com>
2024-04-24 16:32:18 +0200

    InitializerUserDefault

--- Main/InitializeUser.cs
+++ Main/InitializeUser.cs
@@ -11,13 +11,13 @@ namespace Main
         //sdqdqsdqsd
         public async static Task adminInit(DatabaseContext context, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            if (roleManager.FindByNameAsync("ADMIN") == null) // creer un role admin si null
+            if ((await roleManager.FindByNameAsync("ADMIN")) == null) // creer un role admin si null
             {
                 string adminPassword = "Azerty1+";
                 AppUser userAdmin = new AppUser { Email = "admin@admin.fr", NormalizedEmail = "admin@admin.fr", UserName = "admin@admin.fr" };
                 IdentityRole roleAdmin = new IdentityRole { Name = "admin", NormalizedName = "ADMIN" };
 
-                User userClass = new User { AppUserId = userAdmin.Id, Firstname = "admin", AppUser = userAdmin };
+                User userClass = new User { Id= userAdmin.Id , AppUserId = userAdmin.Id, Firstname = "admin", Lastname="ADMIN", AppUser = userAdmin,PictureURL="PHOTO" };
 
                 IdentityRole? roleAdminCheck = await context.Roles.FindAsync(roleAdmin.Id);
                 User? userAdminCheck = await context.Users.FindAsync(userClass.Id);
@@ -27,24 +27,24 @@ namespace Main
                     await userManager.CreateAsync(userAdmin, adminPassword);
                     await roleManager.CreateAsync(roleAdmin);
 
-                    await userManager.AddToRoleAsync(userAdmin, "SUPERADMIN");
+                    await userManager.AddToRoleAsync(userAdmin, "ADMIN");
 
                     await context.AddAsync(userClass);
                     await context.SaveChangesAsync();
                 }
             }
-            Task.Delay(100);
+            
             
         }
         public async static Task UserInit(DatabaseContext context, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            if (roleManager.FindByNameAsync("USER") == null) // Creer un role user si null
+            if ((await roleManager.FindByNameAsync("USER")) == null) // Creer un role user si null
             {
                 string userPassword = "Azerty1+";
                 AppUser newUser = new AppUser { Email = "user@user.fr", NormalizedEmail = "user@user.fr", UserName = "user@user.fr" };
                 IdentityRole roleUser = new IdentityRole { Name = "user", NormalizedName = "USER" };
 
-                User userClass1 = new User { AppUserId = newUser.Id, Firstname = "user", AppUser = newUser };
+                User userClass1 = new User { Id=newUser.Id, AppUserId = newUser.Id, Firstname = "user",Lastname="USER", AppUser = newUser, PictureURL = "PHOTO" };
 
                 IdentityRole? roleUserCheck = await context.Roles.FindAsync(roleUser.Id);
                 User? newUserCheck = await context.Users.FindAsync(userClass1.Id);
