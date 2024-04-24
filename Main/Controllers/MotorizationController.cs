746676be1300d51107a4fbcac58beebaed5dcf03
“[olivierkoe]” <[olikoen@gmail.com]>
2024-04-24 12:52:02 +0200

    Motorization Services Repository Controllers

---
+++ Main/Controllers/MotorizationController.cs
@@ -0,0 +1,25 @@
+﻿using DTO.Motorization;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace Main.Controllers
+{
+        [Route("api/[controller]/[action]")]
+        [ApiController]
+        public class MotorizationController : ControllerBase
+        {
+            public readonly MotorizationServices MotorizationServices;
+            public MotorizationController(MotorizationServices vehicleServices)
+            {
+                MotorizationServices = vehicleServices;
+            }
+
+
+            [HttpPost]
+            public async Task<ActionResult<GetOneMotorizationDTO?>> CreateMotorization(CreateOneMotorizationDTO createMotorizationDTO)
+            {
+                return Ok(await MotorizationServices.CreateMotorizationAsync(createMotorizationDTO));
+            }
+        }
+    }
+
