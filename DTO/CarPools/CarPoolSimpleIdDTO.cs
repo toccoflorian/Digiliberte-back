180ab840cdbdf9b744131a456584850fbc36e292
CedricDLMS <delmascede@gmail.com>
2024-04-25 15:54:17 +0200

    Add of CarPoolSimpleDTO

---
+++ DTO/CarPools/CarPoolSimpleIdDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.CarPools
+{
+    /// <summary>
+    /// Simple CarPoolDTO with only ID 
+    /// </summary>
+    public class CarPoolSimpleIdDTO
+    {
+    }
+}
