5e05c52023a0dee01fd75cabe0dc19842345ba00
Florian <t.florian181181@gmail.com>
2024-04-25 15:14:41 +0200

    fix role par default à la creation d'un user

---
+++ Utils/Constants/ROLE.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Constants
+{
+    public static class ROLE
+    {
+        public static string ADMIN = "ADMIN";
+        public static string USER = "USER";
+    }
+}
