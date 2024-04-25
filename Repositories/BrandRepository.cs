0bbdcb961594cbad2a023e8fabd9e45b6d6a0f0b
“[olivierkoe]” <[olikoen@gmail.com]>
2024-04-25 12:12:47 +0200

    Brand CRUD

--- Repositories/BrandRepository.cs
+++ Repositories/BrandRepository.cs
@@ -42,5 +42,59 @@ namespace Repositories
             };
         }
 
+        public async Task<GetOneBrandDTO?> UpdateOneBrandByIdAsync(GetOneBrandDTO updatedBrandDTO)
+        {
+            // Recherchez le brand existant dans la base de données en fonction de son ID
+            var existingBrand = await Context.Brands.FindAsync(updatedBrandDTO.Id);
+
+            if (existingBrand == null)
+            {
+                // Si le Brnad n'est pas trouvé, vous pouvez choisir de retourner null ou de lever une exception
+                // Ici, je choisis de retourner null
+                throw new Exception("Id not found");
+            }
+
+            // Mettez à jour les propriétés du brand existant avec les nouvelles valeurs
+            existingBrand.Label = updatedBrandDTO.Name;
+
+
+            // Enregistrez les modifications dans la base de données
+            await Context.SaveChangesAsync();
+
+            // Retournez le brand mis à jour sous forme de DTO
+            return new GetOneBrandDTO
+            {
+                Id = existingBrand.Id,
+                Name = existingBrand.Label,
+            };
+        }
+
+        /// <summary>
+        /// Delete a brand by Id , used Id to know if  exists
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public async Task DeleteOneBrandByIdAsync(int brandId)
+        {
+            var brandToDelete = await Context.Brands.FindAsync(brandId);
+
+            if (brandToDelete != null)
+            {
+                Context.Brands.Remove(brandToDelete);
+                await Context.SaveChangesAsync();
+            }
+            // Si le brand n'existe pas, il n'y a rien à supprimer
+        }
+
+        /// <summary>
+        /// Get a brand by Id , used Id to know if  exists already
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public async Task<Brand?> GetOneBrandByIdAsync(int Id)
+        {
+            return await Context.Brands.FirstOrDefaultAsync(c => c.Id == Id);
+        }
+
     }
 }
