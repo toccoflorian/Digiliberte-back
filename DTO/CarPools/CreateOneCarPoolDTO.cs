f78a44abe53362bce7d498ec5e78165d7083b79f
Florian <t.florian181181@gmail.com>
2024-04-28 16:37:50 +0200

    Implementation GetUserByCarPool() and necessaries creates

--- DTO/CarPools/CreateOneCarPoolDTO.cs
+++ DTO/CarPools/CreateOneCarPoolDTO.cs
@@ -5,16 +5,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Models;
+using DTO.Dates;
 
 
 namespace DTO.CarPools
 {
     public class CreateOneCarPoolDTO
     {
+        public string? UserId { get; set; }
         public int RentId { get; set; }
+        public string CarpoolName { get; set; }
+        public int? StartLocalizationId { get; set; }
         public LocalizationDTO StartLocalization { get; set; }
+        public int? EndLocalizationId { get; set; }
         public LocalizationDTO EndLocalization { get; set; }
-        public DatesClass StartDate { get; set; }
-        public DatesClass EndDate { get; set; }
+        public int? StartDateId { get; set; }
+        public GetOneDateDTO StartDate { get; set; }
+        public int? EndDateId { get; set; }
+        public GetOneDateDTO EndDate { get; set; }
     }
 }
---
+++ IServices/ICarPoolPassengerService.cs
@@ -0,0 +1,22 @@
+﻿using DTO.CarPoolPassenger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IServices
+{
+    public interface ICarPoolPassengerService
+    {
+        public Task<GetOneCarPoolPassengerDTO> CreateCarPoolPassengerAsync(CreateCarPoolPassengerDTO createCarPoolPassengerDTO);
+        public Task DeleteCarPoolPassengerByIdAsync(int carPoolPassengerId);
+        public Task<GetOneCarPoolPassengerDTO> UpdateCarPoolPassengerByIdAsync(int carPoolPassengerId);
+        public Task<List<GetOneCarPoolPassengerDTO>> GetPassengerByDescriptionLocalizationAsync(int localizationID);
+        public Task<List<GetOneCarPoolPassengerDTO>> GetPassengerByDescriptionDateAsync(DateTime dateTime);
+        public Task<List<GetOneCarPoolPassengerDTO>> GetPassengersByUserAsync(string userID);
+        public Task<List<GetOneCarPoolPassengerDTO>> GetPassengersByCarPoolAsync(int carPoolID);
+        public Task<List<GetOneCarPoolPassengerDTO>> GetAllPassengersAsync();
+        public Task<GetOneCarPoolPassengerDTO> GetPassengerByIdAsync(int carPoolPassengerID);
+    }
+}
