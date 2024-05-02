16b7b15ebade0322772e9c6bb80780f14c3605eb
Florian <t.florian181181@gmail.com>
2024-05-02 11:22:55 +0200

    [ADD] UpdateCarpoolPassenger()

---
+++ DTO/CarPoolPassenger/UpdateCarPoolPassengerControllerDTO.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace DTO.CarPoolPassenger
+{
+    public class UpdateCarPoolPassengerControllerDTO
+    {
+        public int Id { get; set; }
+        public int CarpoolId { get; set; }
+        public string Description {  get; set; }
+    }
+}
