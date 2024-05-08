ab93e336cbd06ca1e7647564985d002fd46c52a3
CedricDLMS <delmascede@gmail.com>
2024-05-09 00:20:04 +0200

    add develop-chien testest

---
+++ Main/Controllers/testtest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Main.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testtest : ControllerBase
+    {
+    }
+}
